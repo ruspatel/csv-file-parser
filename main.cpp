5711d20df2d9e955b03f1b33a92be92a53e680ef
Rushil Patel <ra8patel@uwaterloo.ca>
2021-08-30 22:07:40 -0400

    wrote basic input parser

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <string>
 
+bool compareStrings(const char* str1, const char* str2){
+    if(strlen(str1) != strlen(str2)){
+        return false;
+    }
 
-int main(){
-    std::cout<<"hello world"<<std::endl;
+    for(int i=0; i < strlen(str1); i++){
+        if(str1[i] != str2[i]){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main(int argc, char** argv){
+    
+    if(argc < 2){
+        std::cout<<"Not enough input parameters"<<std::endl;
+        return -1;
+    }
+
+    const char* findNan = "findNan";
+    const char* calculateMean = "calculateMean";
+
+    if(compareStrings(argv[1], findNan)){
+        std::cout<<"findNan received"<<std::endl;
+    }else if(compareStrings(argv[1], calculateMean)){
+        std::cout<<"calculateMean received"<<std::endl;
+    }
+    
     return 0;
 }
